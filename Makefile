e9018765e15dfc91f625b1f40ab6945c2c4de1f2
William <68653853+will-i-amv@users.noreply.github.com>
2023-08-25 11:57:49 -0400

    Add skeleton of minimal flask app created with cookiecutter, created from https://github.com/candidtim/cookiecutter-flask-minimal/

---
+++ Makefile
@@ -0,0 +1,21 @@
+all: run
+
+clean:
+	rm -rf venv build dist .pytest_cache .mypy_cache *.egg-info
+
+venv:
+	python3 -m venv venv && \
+		venv/bin/pip install --upgrade pip setuptools && \
+		venv/bin/pip install --editable ".[dev]"
+
+run: venv
+	venv/bin/flask --app portfolio-builder --debug run
+
+mypy: venv
+	venv/bin/mypy
+
+test: venv
+	venv/bin/pytest
+
+dist: venv mypy test
+	venv/bin/pip wheel --wheel-dir dist --no-deps .
---
+++ portfolio-builder/__init__.py
@@ -0,0 +1,40 @@
+import logging.config
+
+from flask import Flask
+
+from portfolio-builder import views
+
+
+def configure_logging():
+    logging.config.dictConfig(
+        {
+            "version": 1,
+            "formatters": {
+                "default": {
+                    "format": "[%(asctime)s] %(levelname)s in %(module)s: %(message)s",
+                }
+            },
+            "handlers": {
+                "wsgi": {
+                    "class": "logging.StreamHandler",
+                    "stream": "ext://sys.stdout",
+                    "formatter": "default",
+                }
+            },
+            "root": {"level": "INFO", "handlers": ["wsgi"]},
+        }
+    )
+
+
+def create_app(config_overrides=None):
+    configure_logging()  # should be configured before any access to app.logger
+    app = Flask(__name__)
+    app.config.from_object("portfolio-builder.default_settings")
+    app.config.from_prefixed_env()
+
+    if config_overrides is not None:
+        app.config.from_mapping(config_overrides)
+
+    app.register_blueprint(views.bp)
+
+    return app
