e9ad8e2d413b1364ed44713d5c570cc244c395f0
William <68653853+will-i-amv@users.noreply.github.com>
2023-09-04 14:35:27 -0400

    Changes: * Add migration config files. * Initialize migration extension while creating the app.

--- portfolio_builder/__init__.py
+++ portfolio_builder/__init__.py
@@ -3,6 +3,7 @@ import logging.config
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 from portfolio_builder.default_settings import Config
@@ -12,6 +13,7 @@ db = SQLAlchemy()
 bootstrap = Bootstrap()
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
+migrate = Migrate()
 
 
 def configure_logging():
@@ -44,7 +46,8 @@ def create_app(config_overrides=None):
     db.init_app(app)
     bootstrap.init_app(app)
     login_manager.init_app(app)
-
+    migrate.init_app(app, db)
+    
     if config_overrides is not None:
         app.config.from_mapping(config_overrides)
 
