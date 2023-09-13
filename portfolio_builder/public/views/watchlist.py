d0270100a9119d8296c63ac0fac27568033621fe
William <68653853+will-i-amv@users.noreply.github.com>
2023-09-13 16:41:14 -0400

    Changes: * Add body of function 'add_watchlist()' * Modify function 'index()' to get all available watchlists

--- portfolio_builder/public/views/watchlist.py
+++ portfolio_builder/public/views/watchlist.py
@@ -13,18 +13,43 @@ bp = Blueprint("watchlist", __name__, url_prefix="/watchlist")
 @bp.route("/", methods=('GET', 'POST'))
 @login_required
 def index():
+    watchlists = (
+        db
+        .session
+        .query(Watchlist)
+        .with_entities(Watchlist.name)
+        .filter_by(user_id=current_user.id)
+        .order_by(Watchlist.id)
+        .all()
+    )
     watchlist_select_form = WatchlistSelectForm()
-    watchlist_add_form = WatchlistAddForm()
-    watchlists = []
+    watchlist_select_form.watchlist.choices =  [
+        (item[0], item[0])
+        for item in watchlists
+    ]
     return render_template(
         "public/watchlist.html", 
         watchlist_select_form=watchlist_select_form,
-        watchlist_add_form=watchlist_add_form,
-        watchlists=watchlists
+        watchlist_add_form=WatchlistAddForm(),
     )
 
 
 @bp.route('/add', methods=['POST'])
 @login_required
 def add_watchlist():
+    watchlist_form = WatchlistAddForm()
+    if watchlist_form.validate_on_submit():
+        watchlist_name = watchlist_form.name.data 
+        new_watchlist = Watchlist(
+            name=watchlist_name, 
+            user_id=current_user.id
+        )
+        db.session.add(new_watchlist)
+        db.session.commit()
+        flash(f"The Watchlist group '{watchlist_name}' has been created")
+        return redirect(url_for('watchlist.index'))
+    elif watchlist_form.errors:
+        for error_name, error_desc in watchlist_form.errors.items():
+            error_name = error_name.title()
+            flash(f'{error_name}: {error_desc[0]}')
     return redirect(url_for('watchlist.index'))
