aad941806ff4347f11d4aaf1e243e2f31a7f50cc
William <68653853+will-i-amv@users.noreply.github.com>
2023-09-13 15:47:09 -0400

    Changes: * Add form to create a new watchlist * Modify view function 'index()' and its template to show the new watchlist form * Add skeleton of new function 'add_watchlist()'

--- portfolio_builder/public/forms.py
+++ portfolio_builder/public/forms.py
@@ -1,8 +1,31 @@
+from flask_login import current_user
 from flask_wtf import FlaskForm
 from wtforms import validators as v
-from wtforms import SubmitField, SelectField
+from wtforms import StringField, SubmitField, SelectField
+
+from portfolio_builder.public.models import Watchlist
 
 
 class WatchlistSelectForm(FlaskForm):
     watchlist = SelectField("Select a Watchlist",  validators=[v.InputRequired()])
     submit = SubmitField("Get Overview")
+
+
+class WatchlistAddForm(FlaskForm):
+    name = StringField(
+        "Watchlist Name",
+        validators=[v.InputRequired(), v.Length(min=1, max=25)]
+    )
+    submit = SubmitField("Add")
+
+    def validate_name(self, name):
+        name_check = (
+            Watchlist
+            .query
+            .filter_by(user_id=current_user.id, name=name.data)
+            .first()
+        )
+        if name_check is not None:
+            raise v.ValidationError(
+                "There is already a watchlist with the same name"
+            )
--- portfolio_builder/public/views/watchlist.py
+++ portfolio_builder/public/views/watchlist.py
@@ -1,7 +1,10 @@
-from flask import Blueprint, render_template
+from flask_login import current_user
+from flask import Blueprint, flash, redirect, render_template, url_for
 from flask_login import login_required
 
-from portfolio_builder.public.forms import WatchlistSelectForm
+from portfolio_builder import db
+from portfolio_builder.public.forms import WatchlistAddForm, WatchlistSelectForm
+from portfolio_builder.public.models import Watchlist
 
 
 bp = Blueprint("watchlist", __name__, url_prefix="/watchlist")
@@ -10,10 +13,18 @@ bp = Blueprint("watchlist", __name__, url_prefix="/watchlist")
 @bp.route("/", methods=('GET', 'POST'))
 @login_required
 def index():
-    select_form = WatchlistSelectForm()
+    watchlist_select_form = WatchlistSelectForm()
+    watchlist_add_form = WatchlistAddForm()
     watchlists = []
     return render_template(
         "public/watchlist.html", 
-        form=select_form,
+        watchlist_select_form=watchlist_select_form,
+        watchlist_add_form=watchlist_add_form,
         watchlists=watchlists
     )
+
+
+@bp.route('/add', methods=['POST'])
+@login_required
+def add_watchlist():
+    return redirect(url_for('watchlist.index'))
