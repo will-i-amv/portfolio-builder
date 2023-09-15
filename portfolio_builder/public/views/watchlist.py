b11eafc4f80b8b6035b2aff4c661a29664ba42dd
William <68653853+will-i-amv@users.noreply.github.com>
2023-09-15 12:09:14 -0400

    Changes: * Remove validations of WatchlistAddItemForm, the DB is not filled with securities yet. * Add body of function 'add_watchlist_item' * Add helper function 'get_watchlist_names' * Modify layout of 'watchlist.html' template to use the new select form

--- portfolio_builder/public/views/watchlist.py
+++ portfolio_builder/public/views/watchlist.py
@@ -13,9 +13,7 @@ from portfolio_builder.public.models import Watchlist, WatchlistItem
 bp = Blueprint("watchlist", __name__, url_prefix="/watchlist")
 
 
-@bp.route("/", methods=('GET', 'POST'))
-@login_required
-def index():
+def get_watchlist_names():
     watchlists = (
         db
         .session
@@ -25,14 +23,21 @@ def index():
         .order_by(Watchlist.id)
         .all()
     )
+    return [item[0] for item in watchlists]
+
+
+@bp.route("/", methods=('GET', 'POST'))
+@login_required
+def index():
+    watchlists = get_watchlist_names()
     select_form = WatchlistSelectForm()
     select_form.watchlist.choices =  [
-        (item[0], item[0])
+        (item, item)
         for item in watchlists
     ]
     add_item_form = WatchlistAddItemForm()
     add_item_form.watchlist.choices =  [
-        (item[0], item[0])
+        (item, item)
         for item in watchlists
     ]
     if select_form.validate_on_submit():
@@ -66,6 +71,8 @@ def index():
         select_form=select_form,
         add_watchlist_form=WatchlistAddForm(),
         add_item_form=add_item_form,
+        watchlist_name=watchlist_name,
+        watchlists=watchlists,
         watchlist_items=watchlist_items,
     )
 
@@ -94,4 +101,37 @@ def add_watchlist():
 @bp.route('/add_item', methods=['POST'])
 @login_required
 def add_watchlist_item():
-    return redirect(url_for('watchlist.index'))
+    watchlists = get_watchlist_names()
+    form = WatchlistAddItemForm()
+    form.watchlist.choices = [
+        (item, item)
+        for item in watchlists
+    ]
+    if form.validate_on_submit():
+        watchlist_name = form.watchlist.data
+        ticker = form.ticker.data
+        watchlist = (
+            db
+            .session
+            .query(Watchlist)
+            .filter(Watchlist.name==watchlist_name)
+            .first()
+        )    
+        new_item = WatchlistItem(
+            watchlist=watchlist_name, 
+            ticker=ticker, 
+            quantity=form.quantity.data,
+            price=form.price.data, 
+            sector=form.sector.data,
+            comments=form.comments.data, 
+            watchlist_id=watchlist.id
+        )
+        db.session.add(new_item)
+        db.session.commit()
+        flash(f"{ticker} has been added to your watchlist")
+        return redirect(url_for("watchlist.index"))
+    elif form.errors:
+        for error_name, error_desc in form.errors.items():
+            error_name = error_name.title()
+            flash(f"{error_name}: {error_desc[0]}")
+    return redirect(url_for("watchlist.index"))
