12eca42c461285972b4e4f1746de536cf0a4b0eb
William <68653853+will-i-amv@users.noreply.github.com>
2023-09-19 11:01:56 -0400

    Add env vars for API Keys

--- portfolio_builder/settings.py
+++ portfolio_builder/settings.py
@@ -4,19 +4,24 @@ from dotenv import load_dotenv
 
 
 load_dotenv()
-basedir = os.path.abspath(os.path.dirname(__file__))
 db_user = os.environ.get('DB_USER')
 db_passwd = os.environ.get('DB_PASSWD')
 db_host = os.environ.get('DB_HOST')
 
 
 class Config:
+    ROOT_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev'
+
+    # API keys
+    API_KEY_TIINGO = os.environ.get('API_KEY_TIINGO')
+    API_KEY_EODHD = os.environ.get('API_KEY_EODHD')
 
     # Database Configurations
     DB_URL = f'mysql://{db_user}:{db_passwd}@{db_host}'
     SQLALCHEMY_DATABASE_URI = f'{DB_URL}/main'
     SQLALCHEMY_BINDS = {
-        "Security_Prices": (f'{DB_URL}/prices')
+        "Main": (f'{DB_URL}/main'),
+        "Prices": (f'{DB_URL}/prices'),
     }
     SQLALCHEMY_TRACK_MODIFICATIONS = False
